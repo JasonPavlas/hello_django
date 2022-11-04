841ae8d81c897b49d1711c9081b65232ceec5457
Jason Pavlas <jasonpavlas@jasons-mbp.lan>
2022-11-04 01:31:11 -0400

    updated language to homePage in helloWorld apps url and view

--- django_site/helloWorld/urls.py
+++ django_site/helloWorld/urls.py
@@ -1,6 +1,6 @@
-from django.urls imprt path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homePage, name='homePage'),
 ]
--- django_site/helloWorld/views.py
+++ django_site/helloWorld/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HTTPResponse
+from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World!")
+
+def homePage(request):
+    return HttpResponse('<h1>Hello World</h1>')
